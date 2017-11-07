96973dd9c54442b217a15613979b53f097150eb9
Clément Thiallet <clement.thiallet@gmail.com>
2017-11-07 19:41:29 +0100

    parser transaction input added

--- src/main/java/parser/TransactionInput.java
+++ src/main/java/parser/TransactionInput.java
@@ -1,11 +1,14 @@
 package parser;
 
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+
 public class TransactionInput {
     public byte[] prevTransactionHash;
     public int prevTransactionOutputIndex;
-    public /*var*/int scriptLength;
-    public byte[] scriptSig;
-    public int sequenceNo;
+    public VarInt scriptLength;
+    public byte[] script;
+    public int sequenceNumber;
     public long inputSize;
 
     public TransactionInput() {
@@ -19,6 +22,45 @@ public class TransactionInput {
      * @return parsed TransactionInput object
      */
     public static TransactionInput parseTransactionInput(byte[] transactionInputBytes) {
-        return null;
+    	if (transactionInputBytes == null)
+            return null;
+    	
+    	TransactionInput txIn = new TransactionInput();
+    	ByteBuffer txInBuffer = ByteBuffer.wrap(transactionInputBytes);
+
+        byte[] vi = new byte[9];
+        txInBuffer.get(vi);
+        txIn.scriptLength = new VarInt(vi, 0);
+        txInBuffer.position(txInBuffer.position() - (9 - txIn.scriptLength.size));
+
+        txIn.script = new byte[(int)txIn.scriptLength.value];
+        txInBuffer.get(txIn.script);
+        
+        txInBuffer.get(txIn.prevTransactionHash);
+        txIn.prevTransactionOutputIndex = txInBuffer.getInt();
+        
+        txIn.sequenceNumber = txInBuffer.getInt();
+        
+        txIn.inputSize = 32 + 4 + txIn.scriptLength.size + txIn.scriptLength.value + 4;
+    	
+        return txIn;
+    }
+    
+    static protected byte[] doubleHash(byte[] bytes) throws Exception {
+    	// Get the double hash of byte array
+    	byte[] hash;
+    	MessageDigest sha = MessageDigest.getInstance("SHA-256");
+    	sha.update(bytes);
+    	hash = sha.digest();
+    	sha.update(hash);
+    	return sha.digest();
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\r\nScript: ");
+        sb.append(Helpers.byteArrayToHexString(this.script));
+        return sb.toString();
     }
 }
